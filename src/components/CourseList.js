f782eaeed20965ee08f06021aadfdb817680cf28
Stephanie <stephanieyaur2022@u.northwestern.edu>
2022-10-02 22:10:23 -0500

    Styling

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,7 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const CourseList = ({courseJson}) => {
     var courses = Object.keys(courseJson);
-    console.log(courses);
-    return courses.map(course => <p>{courseJson[course].term} CS {courseJson[course].number}: {courseJson[course].title}</p>)
+    return (
+    <div className="flexContainer">
+        {courses.map(course => 
+            <div className="card m-1 p-2">
+                <div className="card-body">
+                    <h5 className="card-title">{courseJson[course].term} CS {courseJson[course].number}</h5>
+                    <p className="card-text">{courseJson[course].title}</p>
+                    <hr/>
+                    <p className="card-text">{courseJson[course].meets}</p>
+                </div>
+            </div>
+            )}
+    </div>)
 }
 
 export default CourseList;
