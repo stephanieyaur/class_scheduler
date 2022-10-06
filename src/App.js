431e8ef3ab139eea27e1da7577b1aa973a17bc00
Stephanie <stephanieyaur2022@u.northwestern.edu>
2022-10-06 13:19:57 -0500

    Term filter

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import Banner from './components/Banner';
 import CourseList from './components/CourseList';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 
 
+// Variables
+var queryClient = new QueryClient();
+const terms = ['Fall', 'Winter', 'Spring'];
+
+
+// Functions
 const fetchJson = async (url) => {
   const response = await fetch(url);
   if (!response.ok) throw response;
   return response.json();
 };
-
 export const useJsonQuery = (url) => {
   const { data, isLoading, error } = useQuery([url], () => fetchJson(url));
   return [ data, isLoading, error ];
 };
 
+// Components
 const Main = () => {
+  const [selectedTerm, setSelectedTerm] = useState('Fall');
   const url = "https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php";
   var [data, isLoading, error] = useJsonQuery(url);
   if (error) return <h1>Error loading user data: {`${error}`}</h1>;
@@ -25,16 +33,28 @@ const Main = () => {
   return (
     <div>
       <Banner text={data.title} />
-      <CourseList courseJson={data.courses} />
+      {RadioButtons({selectedTerm, setSelectedTerm})}
+      <CourseList courseJson={data.courses} selectedTerm={selectedTerm} />
     </div>
   )
-
 }
-
-var queryClient = new QueryClient();
-
+const RadioButtons = ({ selectedTerm, setSelectedTerm }) => (
+  <div className="radioButtonGroup">
+    <p id="radioButtonTitle">Filter by term:</p>
+    {
+      terms.map(term => 
+        (
+          <div className="radioButton">
+            <input type="radio" id={term} checked={selectedTerm === term} autoComplete="off" onChange={() => setSelectedTerm(term)} />
+            <label className='' htmlFor={term}>{term}</label>
+          </div>
+        )
+      )
+    }  
+  </div>
+)
 const App = () => (
-  <div>
+  <div className="app">
     <QueryClientProvider client={queryClient}>
       <Main />
     </QueryClientProvider>
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,20 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const CourseList = ({courseJson}) => {
+const CourseList = ({ courseJson,selectedTerm }) => {
     var courses = Object.keys(courseJson);
     return (
-    <div className="flexContainer">
-        {courses.map(course => 
-            <div className="card m-1 p-2">
-                <div className="card-body">
-                    <h5 className="card-title">{courseJson[course].term} CS {courseJson[course].number}</h5>
-                    <p className="card-text">{courseJson[course].title}</p>
-                    <hr/>
-                    <p className="card-text">{courseJson[course].meets}</p>
+        <div className="flexContainer">
+            {courses.filter(course => courseJson[course].term == selectedTerm).map(course =>
+                <div className="card m-1 p-2">
+                    <div className="card-body">
+                        <h5 className="card-title">{courseJson[course].term} CS {courseJson[course].number}</h5>
+                        <p className="card-text">{courseJson[course].title}</p>
+                        <hr />
+                        <p className="card-text">{courseJson[course].meets}</p>
+                    </div>
                 </div>
-            </div>
             )}
-    </div>)
+        </div>)
 }
 
 export default CourseList;
