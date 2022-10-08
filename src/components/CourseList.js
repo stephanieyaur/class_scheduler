57865d533f30f53c6a04f116026a9f3903d29be3
Stephanie <stephanieyaur2022@u.northwestern.edu>
2022-10-08 17:32:59 -0500

    Course selection

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,11 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 const CourseList = ({ courseJson,selectedTerm }) => {
     var courses = Object.keys(courseJson);
+    const [selectedClasses, setSelectedClasses] = useState([]);
     return (
         <div className="flexContainer">
             {courses.filter(course => courseJson[course].term == selectedTerm).map(course =>
-                <div className="card m-1 p-2">
+                <div className={selectedClasses.includes(course) ? "card m-1 p-2 green" : "card m-1 p-2"} 
+                    onClick={() => setSelectedClasses(selectedClasses.includes(course) 
+                                                            ? selectedClasses.filter(x => x != course) 
+                                                            : [...selectedClasses, course])}>
                     <div className="card-body">
                         <h5 className="card-title">{courseJson[course].term} CS {courseJson[course].number}</h5>
                         <p className="card-text">{courseJson[course].title}</p>
