5d4476c798298a75f757c5f909be90b006870696
Stephanie <stephanieyaur2022@u.northwestern.edu>
2022-10-20 23:21:31 -0500

    Add form submission

--- src/components/CourseForm.js
+++ src/components/CourseForm.js
@@ -1,6 +1,7 @@
 import { useFormData } from "../utility/useFormData";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { isValidMeet } from "../utility/conflicts";
+import { useDbUpdate } from "../utility/firebase";
 
 const validateCourseForm = (key,val,courseNum, selectedClasses, selectedClassesMeets) => {
     switch (key) {
@@ -24,7 +25,7 @@ const ButtonBar = () => {
     return (
         <div className="d-flex">
         <button type="button" className="btn btn-outline-dark me-2"><Link to={'/'}>Cancel</Link></button>
-        <button type="submit" className="btn btn-primary me-auto" disabled={true}>Submit</button>
+        <button type="submit" className="btn btn-primary me-auto">Submit</button>
         </div>
     );
 }
@@ -33,10 +34,24 @@ const CourseForm = () => {
     const {state} = useLocation();
     const { courseJson, course, selectedClasses, selectedClassesMeets } = state;
     const [stated, change] = useFormData(validateCourseForm, {courseNum: course, courseTitle: courseJson[course].title, meetingTimes: courseJson[course].meets, selectedClasses: selectedClasses, selectedClassesMeets: selectedClassesMeets});
+    const [update, result] = useDbUpdate(`/courses/${course}`);
+    const navigate = useNavigate();
+    const submit = (evt) => {
+        evt.preventDefault();
+        if (!state.errors) {
+            update({
+                meets: stated.values.meetingTimes,
+                number: courseJson[course].number,
+                term: courseJson[course].term,
+                title: stated.values.courseTitle
+            });
+        }
+        navigate('/');
+    };
     return (
         <div>
             <h1>Edit course details</h1>
-            <form noValidate className={stated.errors ? 'was-validated' : null}>
+            <form noValidate className={stated.errors ? 'was-validated' : null} onSubmit={submit}>
                 <InputField name="courseTitle" text="Course Title" state={stated} change={change} />
                 <InputField name="meetingTimes" text="Meeting Times" state={stated} change={change} />
                 <ButtonBar />
